ad0f6d8acb721bad2ab6f5e6601a1fcea42b5995
Yoann <renard.yoann@gmail.com>
2016-10-05 18:31:49 +0200

    Init TestCase

---
+++ src/TestCase/PHPUnit_Annotation_TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace YoannRenard\PHPUnitAnnotation\TestCase;
+
+class PHPUnit_Annotation_TestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @inheritdoc
+     */
+    protected function setUp() {
+        parent::setUp();
+    }
+}
